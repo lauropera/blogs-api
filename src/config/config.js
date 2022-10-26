6beb8a561c2819f5997d610c16910d266bd445f8
Lauro Pereira <lauropera01@gmail.com>
2022-10-25 21:20:26 -0300

    Removendo comentários do docker-compose e arrumando variáveis de ambiente

--- src/config/config.js
+++ src/config/config.js
@@ -9,12 +9,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  database:
     `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
