c8bd881d6056b0345ba45e2c067950a74c846820
Lauro Pereira <lauropera01@gmail.com>
2022-10-29 12:04:16 -0300

    Estrutura inicial swagger

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
 const app = require('./app');
 
+const swaggerDocs = require('./swagger.json');
+
 const port = process.env.API_PORT || 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('/', (_request, response) => {
   response.send();
