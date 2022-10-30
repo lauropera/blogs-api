4d5f8dbe0716f0d29782bb10cbcc1895414f3e86
Lauro Pereira <lauropera01@gmail.com>
2022-10-29 22:15:40 -0300

    Atualizando rota de doc

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const errorMiddleware = require('./middlewares/error');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 const routes = require('./routes/router');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(errorMiddleware);
 
